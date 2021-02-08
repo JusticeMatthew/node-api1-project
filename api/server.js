c34e72e0862db2e12933a7571ee02c6b71741650
Matthew Justice <matthewajustice@gmail.com>
2021-02-08 17:11:03 -0500

    Delete done

--- api/server.js
+++ api/server.js
@@ -5,6 +5,27 @@ const dbFunctions = require('./users/model');
 const app = express();
 app.use(express.json());
 
+app.post('/users', (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ message: 'Please provide name and bio for the user' });
+  } else {
+    dbFunctions
+      .insert({ name, bio })
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: 'There was an error while saving the user to the database',
+        });
+      });
+  }
+});
+
 app.get('/users', (req, res) => {
   dbFunctions
     .find()
@@ -28,24 +49,19 @@ app.get('/users/:id', (req, res) => {
     );
 });
 
-app.post('/users', (req, res) => {
-  const { name, bio } = req.body;
+app.delete('/users/:id', (req, res) => {
+  const tarId = req.params.id;
 
-  if (!name || !bio) {
-    res
-      .status(400)
-      .json({ message: 'Please provide name and bio for the user' });
-  } else {
-    dbFunctions
-      .insert({ name, bio })
-      .then((newUser) => {
-        res.status(201).json(newUser);
-      })
-      .catch(() => {
-        res.status(500).json({
-          message: 'There was an error while saving the user to the database',
-        });
-      });
+  try {
+    dbFunctions.remove(tarId).then((user) => {
+      user === null
+        ? res
+            .status(404)
+            .json({ message: 'The user with the specified ID does not exist' })
+        : res.status(200).json(user);
+    });
+  } catch (e) {
+    res.status(500).json({ message: 'The user could not be removed' });
   }
 });
 
